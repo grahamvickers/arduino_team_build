790112c315e9b4202a5372ef94f81691c2abe7a9
zpereiracollege <z_pereira@fanshaweonline.ca>
2022-03-21 12:10:52 -0400

    Renamed HomeController.php to GuardinoController.php
    Renamed the file name and changed the index to point to guardino

--- Guardino/app/Http/Controllers/GuardinoController.php
+++ Guardino/app/Http/Controllers/GuardinoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 
-class HomeController extends Controller
+class GuardinoController extends Controller
 {
     /**
      * create a new controller instance
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('guardino');
     }
 }
