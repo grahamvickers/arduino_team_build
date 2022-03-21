abdb2decaf451e135e64219ff9d9a52a9512cead
zpereiracollege <z_pereira@fanshaweonline.ca>
2022-03-21 11:06:30 -0400

    Added Sensor Model and create_sensors_table Migration
    Added Model to store the tempature, gas, moisture and light field.
    Added create_sensors_table to create the sensors table with correct type fields

---
+++ Guardino/database/migrations/2022_03_21_145936_create_sensors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSensorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('gas');
+            $table->integer('moisture');
+            $table->integer('temperature');
+            $table->integer('light');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sensors');
+    }
+}
