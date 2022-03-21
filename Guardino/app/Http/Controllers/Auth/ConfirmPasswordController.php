bf6d44d29679285428e4c50ea1a8cd609940838b
zpereiracollege <z_pereira@fanshaweonline.ca>
2022-03-21 12:18:14 -0400

    Updated the Auth Controllers
    updated the Auth Controllers to redirect to the /guardino path instead of root "/"

--- Guardino/app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ Guardino/app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -25,7 +25,7 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/guardino';
 
     /**
      * Create a new controller instance.
