1703cc124b06d46cfe74539a66f7f170ed2f8027
zpereiracollege <z_pereira@fanshaweonline.ca>
2022-03-23 15:58:05 -0400

    Updated App.vue
    Updated App.vue to have router links for path for going to different components. Also added main section with a router view to display a partial section of the other vue components.

--- Guardino/resources/js/components/App.vue
+++ Guardino/resources/js/components/App.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="wrapper">
+        <ul>
+            <li><router-link to="/home">Home</router-link></li>
+            <li><router-link to="/plant">Plant</router-link></li>
+        </ul>
+        <main>
+            <router-view />
+        </main>
     </div>
 </template>
 
