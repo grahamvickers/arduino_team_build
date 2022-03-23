f7eb882f5df18e3bc352cf7122fe6ee8af67adec
zpereiracollege <z_pereira@fanshaweonline.ca>
2022-03-23 16:01:19 -0400

    Updated index.js
    Updated index.js to import the needed components for home and plant and added the route logic for the vue-router.

--- Guardino/resources/js/router/index.js
+++ Guardino/resources/js/router/index.js
@@ -1,8 +1,24 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import Home from '../components/Home.vue';
+import Plant from '../components/Plant.vue';
+
 Vue.use(Router);
 
 export default new Router({
-    routes: []
+    routes: [
+        {
+          path: "*",
+          redirect: "/home"
+        },
+        {
+          path: "/home",
+          component: Home
+        },
+        {
+          path: "/plant",
+          component: Plant
+        }
+      ]
 });
