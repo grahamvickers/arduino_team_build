9148e69adde9f4da0e6aa23d767527c125007f04
zpereiracollege <z_pereira@fanshaweonline.ca>
2022-04-05 14:35:13 -0400

    Updated guardino.blade.php
    Updated the guardino.blade.php to pass a sensor value that is json encoded

--- Guardino/resources/views/guardino.blade.php
+++ Guardino/resources/views/guardino.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 @include('layouts.partials.nav')
     <div id="app">
-        <app></app>
+        <app :sensors="{{ json_encode($sensors) }}"></app>
         <h1>WELCOME USER<h1>
     </div>
 @endsection
