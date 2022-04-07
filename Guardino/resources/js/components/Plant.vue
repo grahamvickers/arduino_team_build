2a5f37924ecd24c9c6a8e9f81094a4beb69bb82e
Graham Vickers <43250387+grahamvickers@users.noreply.github.com>
2022-04-07 00:49:13 -0400

    adding in twillo sms

--- Guardino/resources/js/components/Plant.vue
+++ Guardino/resources/js/components/Plant.vue
@@ -193,6 +193,8 @@
                 document.getElementById('myChart'),
                 config
             );
+
+           
         },
         methods: {
             postVal() {
@@ -204,7 +206,25 @@
                     console.log(response.status);
                 });
 
-            }
+            },
+
+            // sendSms() {
+            //     // sms function below 
+            //     const client = require('twilio')('ACe0066d7fe8e4e129d320d77a991d0237', '5170db6740df41b9b1d4ae6c57fbbd9d');
+
+            //     if (vm.moisture >= 85) {
+            //         client.messages
+            //         .create({
+            //             body: 'Flora.io plant alert!',
+            //             // twillo sending number
+            //             from: '+15078794673',
+            //             // user number
+            //             to: '+12269270901'
+            //         }).then(message => console.log(message))
+            //         .catch(error => console.log(error))
+            //     }
+                
+            // }
         }
     }
 </script>
